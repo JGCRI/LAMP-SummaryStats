6215e8eca8c09d479978e80a7004b03e65b33dd7
Anupriya Mundra <anupriya.mundra@pnnl.gov>
2015-11-12 17:45:01 -0500

    Adding file for calculating summary stats for precipitation, and a file for making figures, and docfix for rest of code

---
+++ summary-stat-pcp.R
@@ -0,0 +1,121 @@
+
+## This script takes CMIP5 data and calculates a spatial average over S.America
+## The final product is a csv file of all models by month(?).
+library(RCMIP5)
+library(ncdf4)
+library(dplyr)
+
+
+path <-  "/pic/projects/GCAM/CMIP5-CHartin"
+areapath <- "/pic/projects/GCAM/CMIP5-anupriya/Landareas"
+landfracpath <- "/pic/projects/GCAM/CMIP5-anupriya/Landfrac"
+OUTPUT_DIR <- "/people/mund663/work/LAMP-SummaryStats/output/"
+
+experiments <- c( "historical", "rcp26", "rcp45", "rcp60", "rcp85")
+varlist <- c("pr")
+areavar <- "areacella"
+landfracvar <- "sftlf"
+modellist <- c("ACCESS1-0","ACCESS1-3","BNU-ESM","CCSM4","CESM1-BGC","CESM1-CAM5","CESM1-FASTCHEM","CESM1-WACCM","CMCC-CESM",
+               "CMCC-CMS","CMCC-CM","CNRM-CM5-2","CNRM-CM5","CSIRO-Mk3-6-0","CSIRO-Mk3L-1-2","FGOALS-g2","FIO-ESM",
+               "GFDL-CM3","GFDL-ESM2G","GFDL-ESM2M","HadGEM2-AO","HadGEM2-CC","HadGEM2-ES","IPSL-CM5A-LR",
+               "IPSL-CM5A-MR","IPSL-CM5B-LR","MIROC-ESM-CHEM","MIROC-ESM","MIROC4h","MIROC5","MPI-ESM-LR","MPI-ESM-MR",
+               "MPI-ESM-P","MRI-CGCM3","MRI-ESM1","bcc-csm1-1-m","bcc-csm1-1","inmcm4","ACCESS1-0","EC-EARTH",
+               "GISS-E2-H-CC","GISS-E2-H","GISS-E2-R-CC","GISS-E2-R","NorESM1-ME","NorESM1-M")
+#"r1i1p1", 
+ensemble_list <- c("r1i1p1", "r2i1p1", "r3i1p1", "r4i1p1", "r5i1p1", "r6i1p1", "r7i1p1", "r8i1p1", "r9i1p1", "r10i1p1")
+
+convert_todf <- function(d, vname=d$variable, suffix="") {
+  
+  df <- as.data.frame(d)    
+  df$units <- d$valUnit
+  df$variable <- paste0(vname, suffix)
+  df$scenario <- d$experiment
+  df$type <- "Simulated"
+  df$model <- d$model
+  df
+  
+}
+
+filelist <- getFileInfo(path=path )
+
+for (e in ensemble_list){
+  # loop counter
+  first=T
+  for (v in varlist){
+    for (m in modellist){
+      for (ex in experiments){
+      print("================================================")
+      print (v)
+      print(m)
+      print(ex)
+      print("================================================")
+      
+      # load area file 
+      # why does this load as a list and not as a CMIP data object? 
+      larea <- loadCMIP5(variable=areavar, model=m, experiment="historical", 
+                         path=areapath, ensemble="r0i0p0", domain="fx",verbose=T, recursive=F)
+      
+      lfrac <- loadCMIP5(variable=landfracvar, model=m, experiment="historical", 
+                         path=landfracpath, ensemble="r0i0p0", domain="fx",verbose=T, recursive=T)
+      
+      # stop if area or landfrac file doesnt exist because cant calculate weighted avg without area
+      # this script shouuld be able to calculate total area of cells, but glitchy right now
+      
+      if(is.null(larea)){
+        next    
+      }
+      if(is.null(lfrac)){
+        next    
+      }
+     
+      ## only land areas
+      print("calculating land only area of cell")
+      larea.frac <- larea
+      larea.frac$val$value <- larea$val$value * lfrac$val$value
+      
+      # load data file
+      d <- loadCMIP5( variable=v, model=m, experiment=ex, ensemble= e, path=path, verbose=T)
+      
+      if (is.null(d)) next
+
+      # There is a problem with duplicate files in our directories that are not detected by RCMIP5
+      # Until this is fixed, handle it ourselves
+      if(nrow(subset(filelist,variable==v & model==m & ensemble==e & experiment==ex)) > 1) {
+        print("More than one file present! Averaging")
+        d$val <- group_by(d$val, lon, lat, Z, time) %>% summarise(value=mean(value))
+      }
+      
+      ## filter to desired spatial coordinates -- Latin America for now
+      print("filter temp to Latin America")
+      # longitudes are in 0-360 degree with 0 being in the pacific. 
+      dim_lam <- filterDimensions(d, lats=d$lat[d$lat >= -60 & d$lat <=30],
+                                lons=d$lon[d$lon >= 60 & d$lon <=150])
+      area_lam <- filterDimensions(larea.frac, lats=larea.frac$lat[larea.frac$lat >= -60 & larea.frac$lat <=30], 
+                                 lons=larea.frac$lon[larea.frac$lon >= 60 & larea.frac$lon <=150])
+
+
+      ## fix the decimal places of lat, lon variables in both area and data files
+      ## they need to be identical, otherwise global avg function fails.
+      print("round the lat lon numbrs")
+      dim_lam$lat <- round(dim_lam$lat, digits = 2)
+      dim_lam$lon <- round(dim_lam$lon, digits = 2)
+      area_lam$lat <- round(area_lam$lat, digits = 2)
+      area_lam$lon <- round(area_lam$lon, digits = 2)
+     
+      ## take spatial average
+      print("take spatial average over s.america")
+      lam_avg <- makeGlobalStat(dim_lam, area=area_lam, verbose=T, FUN=weighted.mean, na.rm=TRUE)
+     
+      pr_lam <- convert_todf(lam_avg, vname=v)
+      pr_lam$ensemble <- e
+      
+      # write file out, append to same file as you go along
+      outfnm <- paste0( OUTPUT_DIR, v, "_", ex, "_", e, ".csv" )
+      write.table( pr_lam, file=outfnm, row.names=F, sep=",", col.names=first, append=!first)
+      first=FALSE
+	rm (d, larea, lfrac, larea.frac, dim_lam, area_lam, lam_avg, pr_lam)
+gc()
+    }
+  }
+}
+}
--- temporal_avg.R
+++ temporal_avg.R
@@ -6,6 +6,9 @@ options(stringsAsfactors=FALSE)
 # all temperature files (temperature data is spread over different files)
 files <- list.files(path="~/LAMP-summary-stat/output/", pattern=glob2rx("tas_*.csv"), full.names=T)
 
+## function reads in csv file based on whether it has a column names specified or not
+# by checking if the first row has "time" as a column name for the second column.
+# f: filename 
 read.csv.mystyle <- function(f) {
   a <- scan(f, what="complex", nmax=3, sep=",")
   h <- ifelse(a[[2]]=="time", TRUE, FALSE) # some input files heave headers, some dont, my bad.
@@ -15,7 +18,9 @@ read.csv.mystyle <- function(f) {
   return(dat)
 }
 
+# read all the files with temperature data and make a list
 d.list <- lapply(files, read.csv.mystyle)
+# combine items of above list into one data frame
 d <- rbind_all(d.list)
 
 #extract year and month from time 
@@ -26,9 +31,11 @@ d$month <- floor((d$time - d$year) * 12) + 1
 #clean to keep only columns needed
 d <- d[c("year", "month", "value", "units", "variable", "scenario", "model", "ensemble")]
 
-d <- as.data.frame(d)
+# rbind_all gives a dplyr data frame which has some strange idiosyncrasies. convert to normal df
+d <- as.data.frame(d) 
 
-# write.csv(d, "temperature.csv")
+# write out temperature data. 
+write.csv(d, "temperature.csv") 
 
 ###########################
 #############################
@@ -36,173 +43,109 @@ d <- as.data.frame(d)
 
 #DECADAL MEAN
 
-d <- read.csv("temperature.csv")
+# Split data into decades. cmpi5 temperature starts in 1850.
+d1 <- d
+d1 <- subset(d1, year < 2101)
+d1$decade <- cut(d1$year, breaks=seq(1850, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1860,2100,10))
 
-# Split data into decades
-d$decade <- cut(d$year, breaks=seq(1850, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1860,2100,10))
-
-# calculate decadal mean for each month
-results.grouped <- group_by( d, model, scenario, ensemble, month, decade )
+# calculate decadal mean by month for each month
+results.grouped <- group_by( d1, model, scenario, ensemble, month, decade )
 results <- dplyr::summarise( results.grouped, 
-                             decadal.mean=mean(value))
+                             decadal.mean.bymonth=mean(value))
+
+# summarise gives a dplyr data frame which has some strange idiosyncrasies. convert to normal df
 results <- as.data.frame(results)
 
-# write.csv(results, file="temperature-decadalmean.csv", row.names=F)
+#match in decadal mean by month into d
+d1$decadal.mean.bymonth <- results [ match(paste0( d1$month, d1$scenario, d1$model, d1$ensemble, d1$decade ), 
+                                  paste0( results$month, results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean.bymonth"]
 
-#match in decadal mean into d
-d$decadal.mean <- results [ match(paste0( d$month, d$scenario, d$model, d$ensemble, d$decade ), 
-                                  paste0( results$month, results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean"]
+# calculate seasonally adjusted variance 
+d1$variance <- (d1$value - d1$decadal.mean.bymonth)^2
 
-# calculate decadal variance. 
-d$variance <- (d$value - d$decadal.mean)^2
-
-results.grouped <- group_by( d, model, scenario, ensemble, month, decade )
+results.grouped <- group_by( d1, model, scenario, ensemble,  decade )
 results.var <- dplyr::summarise( results.grouped, 
-                             decadal.variance=sum(variance))
+                                 seasonally.adjusted.variance=sum(variance))
 results.var <- as.data.frame(results.var)
 
-results.var$decadal.variance <- as.numeric(results.var$decadal.variance) / 120
+results.var$seasonally.adjusted.variance <- results.var$seasonally.adjusted.variance / 120
+
+## calculate total, actual decadal mean
+d2 <- d
+d2$decade <- cut(d2$year, breaks=seq(1850, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1860,2100,10))
+
+results.grouped <- group_by( d2, model, scenario, ensemble, decade )
+results <- dplyr::summarise( results.grouped, 
+                             decadal.mean=mean(value))
+results<- as.data.frame(results)
 
 # match decadal mean back into table with decadal variance (how to do in one step in dplyr?)
-results.var$decadal.mean <- results [ match(paste0( results.var$month, results.var$scenario, results.var$model, results.var$ensemble, results.var$decade ), 
-                                            paste0( results$month, results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean"]
+results.var$decadal.mean <- results [ match(paste0(  results.var$scenario, results.var$model, results.var$ensemble, results.var$decade ), 
+                                            paste0(  results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean"]
 
-# write.csv
+results.var$seasonally.adjusted.standard.deviation <- sqrt(results.var$seasonally.adjusted.variance)
+results.var$seasonally.adjusted.variance <- NULL
+
+write.csv(results.var, file="decadal-temperature.csv", row.names=F)
 ###########################################
 ###########################################
 ###########################################
 
 ## RUNOFF ###
 
-r <- read.csv("~/LAMP-SummaryStats/runoff.csv")
+r <- read.csv("c:/LAMP-SummaryStats/runoff.csv")
 
-r$decade <- cut(r$year, breaks=seq(1950, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1960,2100,10))
+# Split data into decades. runoff starts in 1950
+d1 <- r
+d1$decade <- cut(d1$year, breaks=seq(1950, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1960,2100,10))
 
-results.grouped <- group_by( r, model, scenario, ensemble, month, decade )
+# calculate decadal mean by month for each month
+results.grouped <- group_by( d1, model, scenario, ensemble, month, decade )
+results <- dplyr::summarise( results.grouped, 
+                             decadal.mean.bymonth=mean(value))
+
+# summarise gives a dplyr data frame which has some strange idiosyncrasies. convert to normal df
+results <- as.data.frame(results)
+
+#match in decadal mean by month into d
+d1$decadal.mean.bymonth <- results [ match(paste0( d1$month, d1$scenario, d1$model, d1$ensemble, d1$decade ), 
+                                           paste0( results$month, results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean.bymonth"]
+
+# calculate seasonally adjusted variance 
+d1$variance <- (d1$value - d1$decadal.mean.bymonth)^2
+
+results.grouped <- group_by( d1, model, scenario, ensemble, decade )
+results.var <- dplyr::summarise( results.grouped, 
+                                 seasonally.adjusted.variance=sum(variance))
+results.var <- as.data.frame(results.var)
+
+results.var$seasonally.adjusted.variance <- results.var$seasonally.adjusted.variance / 120
+
+## calculate total, actual decadal mean
+d2 <- r
+d2$decade <- cut(d2$year, breaks=seq(1950, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1960,2100,10))
+
+results.grouped <- group_by( d2, model, scenario, ensemble, decade )
 results <- dplyr::summarise( results.grouped, 
                              decadal.mean=mean(value))
+results<- as.data.frame(results)
 
-write.csv(results, file="runoff-decadalmean.csv", row.names=F)
+# match decadal mean back into table with decadal variance (how to do in one step in dplyr?)
+results.var$decadal.mean <- results [ match(paste0(  results.var$scenario, results.var$model, results.var$ensemble, results.var$decade ), 
+                                            paste0(  results$scenario, results$model, results$ensemble, results$decade )), "decadal.mean"]
+results.var$seasonally.adjusted.standard.deviation <- sqrt(results.var$seasonally.adjusted.variance)
+results.var$seasonally.adjusted.variance <- NULL
+
+
+write.csv(results.var, file="decadal-runoff.csv", row.names=F)
 
 ##############################################
 ###############################################
-###Figures Decadal Means####
-
-d <- read.csv("runoff-decadalmean.csv")
-
-scenario <- c("rcp26", "rcp45", "rcp60", "rcp85")
-month <- 1:12
 
 
-for (s in scenario){
-  for (t in month){
-    
-    d.fig <- subset(d, month == t & (scenario == s|scenario=="historical"))
-    
-    p <- ggplot() + geom_line(data=d.fig, aes(decade, decadal.mean, color=model), size=1.25) + 
-      scale_x_continuous(limits=c(1960,2100), breaks = seq(1960,2100, by=20), expand = c(0,0)) +
-      scale_y_continuous(limits=c(0,0.25), breaks = seq(0,0.5, by=0.05), expand = c(0, 0)) #+
-    #coord_cartesian(xlim=c(2010, 2250))+ coord_cartesian(ylim=c(0,10))
-    p <- p + ylab("Decadal Monthly Mean Runoff") + xlab(paste0 ("month:", t)) + 
-
-      
-      scale_colour_manual(name = "model", values = color.1) +
-      
-      theme( legend.position= "right",
-             legend.text = element_text(size = 18), 
-             axis.text.x = element_text(size = 30, color="black", angle = 90, hjust=0.5, vjust=0.5), 
-             axis.text.y = element_text(size = 34, color="black"),
-             #legend.title = element_blank(),
-             plot.title = element_text(size = 56, face="bold"),
-             axis.title.x = element_blank(), 
-             axis.title.y = element_text(size = 36, face="bold", vjust=0.5), 
-             # legend.key = element_blank(),
-             legend.title = element_blank(),
-             panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             panel.border  = element_rect(color="black", fill=NA))
-    
-    ggsave(filename = paste0("runoff_", s, "_", t, ".png"),  width = 400, height = 297, units = "mm" ) 
-  }
-}
-###########
-color.1 = c("#000000","#1CE6FF","#FF34FF","#FF4A46","#008941","#006FA6","#A30059","#FFDBE5","#7A4900","#0000A6",
-            "#63FFAC","#B79762","#004D43","#8FB0FF","#997D87","#5A0007","#809693","#FEFFE6","#1B4400","#4FC601","#3B5DFF",
-            "#4A3B53","#FF2F80","#61615A","#BA0900","#6B7900","#00C2A0","#FFAA92","#FF90C9","#B903AA","#D16100","#DDEFFF",
-            "#000035","#7B4F4B","#A1C299","#300018","#0AA6D8","#013349","#00846F","#372101","#FFB500","#C2FFED","#A079BF",
-            "#CC0744","#C0B9B2","#C2FF99","#001E09","#00489C","#6F0062","#0CBD66","#EEC3FF","#456D75","#B77B68","#7A87A1",
-            "#788D66","#885578","#FAD09F","#FF8A9A","#D157A0","#BEC459","#456648","#0086ED","#886F4C","#34362D","#B4A8BD",
-            "#00A6AA","#452C2C","#636375","#A3C8C9","#FF913F","#938A81","#575329","#00FECF","#B05B6F","#8CD0FF","#3B9700",
-            "#04F757","#C8A1A1","#1E6E00","#7900D7","#A77500","#6367A9","#A05837","#6B002C","#772600","#D790FF","#9B9700",
-            "#549E79","#FFF69F","#201625","#72418F","#BC23FF","#99ADC0","#3A2465","#922329","#5B4534","#FDE8DC","#404E55",
-            "#0089A3","#CB7E98","#A4E804","#324E72","#6A3A4C","#83AB58","#001C1E","#D1F7CE","#004B28","#C8D0F6","#A3A489",
-            "#806C66","#222800","#BF5650","#E83000","#66796D","#DA007C","#FF1A59","#8ADBB4","#1E0200","#5B4E51","#C895C5",
-            "#320033","#FF6832","#66E1D3","#CFCDAC","#D0AC94","#7ED379","#012C58","#7A7BFF","#D68E01","#353339","#78AFA1",
-            "#FEB2C6","#75797C","#837393","#943A4D","#B5F4FF","#D2DCD5","#9556BD","#6A714A","#001325","#02525F","#0AA3F7",
-            "#E98176","#DBD5DD","#5EBCD1","#3D4F44","#7E6405","#02684E","#962B75","#8D8546","#9695C5","#E773CE","#D86A78",
-            "#3E89BE","#CA834E","#518A87","#5B113C","#55813B","#E704C4","#00005F","#A97399","#4B8160","#59738A","#FF5DA7",
-            "#F7C9BF","#643127","#513A01","#6B94AA","#51A058","#A45B02","#1D1702","#E20027","#E7AB63","#4C6001","#9C6966",
-            "#64547B","#97979E","#006A66","#391406","#F4D749","#0045D2","#006C31","#DDB6D0","#7C6571","#9FB2A4","#00D891",
-            "#15A08A","#BC65E9","#FFFFFE","#C6DC99","#203B3C","#671190","#6B3A64","#F5E1FF","#FFA0F2","#CCAA35","#374527",
-            "#8BB400","#797868","#C6005A","#3B000A","#C86240","#29607C","#402334","#7D5A44","#CCB87C","#B88183","#AA5199",
-            "#B5D6C3","#A38469","#9F94F0","#A74571","#B894A6","#71BB8C","#00B433","#789EC9","#6D80BA","#953F00","#5EFF03",
-            "#E4FFFC","#1BE177","#BCB1E5","#76912F","#003109","#0060CD","#D20096","#895563","#29201D","#5B3213","#A76F42",
-            "#89412E","#1A3A2A","#494B5A","#A88C85","#F4ABAA","#A3F3AB","#00C6C8","#EA8B66","#958A9F","#BDC9D2","#9FA064",
-            "#BE4700","#658188","#83A485","#453C23","#47675D","#3A3F00","#061203","#DFFB71","#868E7E","#98D058","#6C8F7D",
-            "#D7BFC2","#3C3E6E","#D83D66","#2F5D9B","#6C5E46","#D25B88","#5B656C","#00B57F","#545C46","#866097","#365D25",
-            "#252F99","#00CCFF","#674E60","#FC009C","#92896B")
-
-#################
-
-### temperature charts
 
 
-d <- read.csv("temperature-decadalmean.csv")
-
-scenario <- c("rcp26", "rcp45", "rcp60", "rcp85")
-month <- 1:12
 
 
-for (s in scenario){
-  for (t in month){
-    
-    d.fig <- subset(d, month == t & ensemble=="r1i1p1" & (scenario == s|scenario=="historical"))
-    
-    p <- ggplot() + geom_line(data=d.fig, aes(decade, decadal.mean, color=model), size=1.25) + 
-      scale_x_continuous(limits=c(1860,2100), breaks = seq(1860,2100, by=40), expand = c(0,0)) +
-      scale_y_continuous(limits=c(290,310), breaks = seq(290,310, by=2), expand = c(0, 0)) #+
-    #coord_cartesian(xlim=c(2010, 2250))+ coord_cartesian(ylim=c(0,10))
-    p <- p + ylab("Decadal Monthly Mean Runoff") + xlab(paste0 ("month:", t)) + 
-      
-      scale_colour_manual(name = "model", values = color.1) +
-      
-      theme( legend.position= "right",
-             legend.text = element_text(size = 18), 
-             axis.text.x = element_text(size = 30, color="black", angle = 90, hjust=0.5, vjust=0.5), 
-             axis.text.y = element_text(size = 34, color="black"),
-             #legend.title = element_blank(),
-             plot.title = element_text(size = 56, face="bold"),
-             axis.title.x = element_blank(), 
-             axis.title.y = element_text(size = 36, face="bold", vjust=0.5), 
-             # legend.key = element_blank(),
-             legend.title = element_blank(),
-             panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-             panel.background = element_blank(),
-             panel.border  = element_rect(color="black", fill=NA))
-    
-    ggsave(filename = paste0("temperature_", s, "_", t, ".png"),  width = 400, height = 297, units = "mm" ) 
-  }
-}
-
-
-#################################
-## Variance ###
-
-d$decade <- cut(d$year, breaks=seq(1850, 2100, 10), dig.lab=5, include.lowest=T, label=seq(1860,2100,10))
-
-
-results.grouped <- group_by( r, model, scenario, ensemble, month, decade )
-results <- dplyr::summarise( results.grouped, 
-                             decadal.mean=mean(value))
 
 
